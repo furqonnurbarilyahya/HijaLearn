897ba106d1f29a03005c4354380b74a14dbf3e63
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-12-11 11:42:43 +0700

    add al-qur'an page and all material page

---
+++ app/src/main/java/com/bangkit/hijalearn/ui/screen/allmaterial/AllMaterialScreen.kt
@@ -0,0 +1,15 @@
+package com.bangkit.hijalearn.ui.screen.allmaterial
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AllMaterialScreen(
+    modifier: Modifier = Modifier
+) {
+    Column {
+        Text(text = "Ini adalah halaman untuk daftar semua materi")
+    }
+}
