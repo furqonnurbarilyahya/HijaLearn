4d32d813765a5e6300022259bd0313473cc1c34b
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-12-11 11:41:00 +0700

    add new component

--- app/src/main/java/com/bangkit/hijalearn/model/BottomBarItem.kt
+++ app/src/main/java/com/bangkit/hijalearn/model/BottomBarItem.kt
@@ -1,8 +1,10 @@
 package com.bangkit.hijalearn.model
 
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.bangkit.hijalearn.navigation.Screen
 
 data class BottomBarItem(
     val title: String,
-    val icon: ImageVector
+    val icon: ImageVector,
+    val screen: Screen
 )
