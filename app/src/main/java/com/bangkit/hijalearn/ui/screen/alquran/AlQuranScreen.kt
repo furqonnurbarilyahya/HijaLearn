57a33539d7b0a326ed2afa835530cd13c639bec4
Furqon Nurbaril Yahya <furqonyahya1@gmail.com>
2023-12-14 10:11:36 +0700

    add ListSurahResponse class

--- app/src/main/java/com/bangkit/hijalearn/ui/screen/alquran/AlQuranScreen.kt
+++ app/src/main/java/com/bangkit/hijalearn/ui/screen/alquran/AlQuranScreen.kt
@@ -1,174 +1,79 @@
 package com.bangkit.hijalearn.ui.screen.alquran
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import android.content.Context
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Arrangement.Center
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.bangkit.hijalearn.R
-import com.bangkit.hijalearn.ui.screen.profile.ProfileScreen
-import com.bangkit.hijalearn.ui.theme.HijaLearnTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bangkit.hijalearn.MainViewModelFactory
+import com.bangkit.hijalearn.data.UiState
+import com.bangkit.hijalearn.di.Injection
+import com.bangkit.hijalearn.model.ListSurahResponseItem
 
 @Composable
-fun AlQuranScreen() {
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.primary),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Spacer(modifier = Modifier.height(40.dp))
-        Image(
-            painter = painterResource(id = R.drawable.icon_category_espresso),
-            contentDescription = null,
-            modifier = Modifier
-                .width(120.dp)
-                .height(120.dp)
-                .padding(vertical = 10.dp)
-        )
-        Text(
-            text = "Username",
-            color = Color.White,
-            fontSize = 26.sp,
-            fontWeight = FontWeight.SemiBold
-        )
-        Spacer(modifier = Modifier.height(30.dp))
-        Column (
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = colorResource(id = R.color.white2))
-                .clip(shape = RoundedCornerShape(20.dp))
-        ) {
-            Spacer(modifier = Modifier.height(20.dp))
-            Text(
-                text = "Informasi Pribadi",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.SemiBold,
-                modifier = Modifier.padding(start = 20.dp)
-            )
-            Spacer(modifier = Modifier.height(10.dp))
-            Column (
-                modifier = Modifier
-                    .background(color = Color.White)
-            ) {
-                Row (
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { }
-                        .padding(horizontal = 20.dp, vertical = 20.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        text = "Profil",
-                        fontSize = 16.sp
-                    )
-                    Text(text = ">")
-                }
-                Divider(thickness = 0.6.dp, color = Color.Black)
-                Row (
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { }
-                        .padding(horizontal = 20.dp, vertical = 20.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        text = "Akun saya",
-                        fontSize = 16.sp
-                    )
-                    Text(text = ">")
-                }
-            }
-            Spacer(modifier = Modifier.height(15.dp))
-            Text(
-                text = "Lainnya",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.SemiBold,
-                modifier = Modifier.padding(start = 20.dp)
-            )
-            Spacer(modifier = Modifier.height(10.dp))
-            Column (
-                modifier = Modifier
-                    .background(color = Color.White)
-            ) {
-                Row (
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { }
-                        .padding(horizontal = 20.dp, vertical = 20.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        text = "Bantuan",
-                        fontSize = 16.sp,
+fun AlQuranScreen(
+    context: Context,
+    alQuranViewModel: AlQuranViewModel =  viewModel(factory = MainViewModelFactory(Injection.provideMainRepository(context))),
+) {
 
-                        )
-                    Text(text = ">")
-                }
-                Divider(thickness = 0.6.dp, color = Color.Black)
-                Row (
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { }
-                        .padding(horizontal = 20.dp, vertical = 20.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween
+    alQuranViewModel.listSurahState.collectAsState(initial = UiState.Loading).value.let {
+        when (it) {
+            is UiState.Loading -> {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Center,
+                    horizontalAlignment = CenterHorizontally
                 ) {
-                    Text(
-                        text = "Tentang aplikasi",
-                        fontSize = 16.sp
+                    CircularProgressIndicator(
+                        color = MaterialTheme.colorScheme.primary
                     )
-                    Text(text = ">")
                 }
+                alQuranViewModel.getListSurah()
             }
-            Spacer(modifier = Modifier.height(20.dp))
-            Button(
-                onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.error
-                ),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp)
-            ) {
-                Text(
-                    text = "Keluar",
-                    fontSize = 16.sp
+
+            is UiState.Success -> {
+                val surah = it.data
+                //content
+                ListSurahItem(
+                    surahItem = surah
                 )
             }
+
+            else -> {}
         }
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true, device = Devices.PIXEL_3A)
 @Composable
-fun ProfileScreenPreview() {
-    HijaLearnTheme {
-        AlQuranScreen()
+fun ListSurahItem(
+    surahItem: List<ListSurahResponseItem>,
+) {
+    LazyColumn(
+        modifier = Modifier.padding(10.dp)
+    ) {
+        items(surahItem) {
+            Card (modifier = Modifier.fillMaxWidth()) {
+                Text(text = it.nama)
+            }
+            Spacer(modifier = Modifier.height(10.dp))
+        }
     }
 }
